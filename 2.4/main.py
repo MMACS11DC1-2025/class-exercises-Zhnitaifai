5f8a6c8380f222bcaa5f2495d2cf1df2580caf57
zhnitaifai <brendanzjy@gmail.com>
2025-09-29 14:18:52 -0700

    2.5 testing

--- 2.4/main.py
+++ 2.4/main.py
@@ -1,1 +1,8 @@
 file = open("2.4/responses.csv")
+
+DATA = file.read().split(",")
+
+for line in DATA:
+    if "brendan yap" in line.lower():
+        print(line)
+        break
--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -6,3 +6,32 @@ You may use user input to add interactivity to the program.
 You must design your algorithm in English first, then translate it to Python code.
 Test as you go! Describe in your comments what steps you took to test your code.
 """
+# get people wtih same sports
+# enter name
+# ask category the is similar
+
+file = open("2.4/responses.csv")
+file.readline()
+
+fatData = []
+
+for line in file:
+    fatData.append(line.split(","))
+
+data = []
+data.append(fatData[0].split(","))
+# fatData = file.read().strip(",")
+print( )
+
+for i in range(int(len(fatData)/10)):
+    if fatData[i-(i*10)+6] == fatData[i-(i*10)+7]:
+        print(fatData[i-(i*10)])
+
+print(len(fatData)/10)
+# for i in range(len(fatData)):
+#     for each in fatData:
+#         if i > 10:
+#             if (i-(i*10)) % 6 == 0:
+
+#         else:
+#             dfddfdf
