148ccc6d4220f616a568496b0d6af47d074f39f0
zhnitaifai <165229380+Zhnitaifai@users.noreply.github.com>
2025-10-08 20:40:52 +0000

    Completely redesign main algorithm

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -17,8 +17,7 @@ Test as you go! Describe in your comments what steps you took to test your code.
 
 # test your own similarities
 # give suggestion from fav fast food (including people to go with and dish)
-# find if like watching same sport as playing (not necessary)
-
+# find personal suggestions for specific person
 
 # init stuf
 file = open("2.4/responses.csv")
@@ -37,87 +36,64 @@ while userLine == 0:
             userLine = lines
 
 
-numberSimilarities = []
-nameSimilarities = []
+# similarThingsNameIndex = []
+# similarThings = []
+
+# for person in range(len(fatData)):
+#     for i in range(len(fatData[person])):
+#         if fatData[userLine][i] == fatData[person][i]:
+#             similarThingsNameIndex.append(person)
+#             similarThings.append(fatData[person][i])
+
+# print(similarThingsNameIndex)
+# print(similarThings)
+
+# num = len(similarThingsNameIndex)-(len(similarThingsNameIndex)-10)
+# for each in range(num):
+#     del similarThingsNameIndex[len(similarThingsNameIndex)-1]
+#     del similarThings[len(similarThings)-1]
+
+# print(similarThingsNameIndex)
+
+# numOfSimilarAccumalative = 0
+# for each in range(len(similarThingsNameIndex)):
+#     repeats = {}
+
+#     for i in similarThings:
+#         if i in repeats:
+#             repeats[i] += 1
+#         else:
+#             repeats[i] = 1
+    
+#     # get similar person
+#     similarPerson = fatData[similarThingsNameIndex[int(each)]][1]
+#     print(f"You have {repeats[int(each)]} similarities with {similarPerson}")
+
+similaritiesSentences = []
+similarPeople = []
 
 for person in range(len(fatData)):
-    for i in range(len(fatData[person])):
-        if fatData[userLine][i] == fatData[person][i]:
-            numberSimilarities.append(person)
-            nameSimilarities.append(fatData[person][i])
+    if person == userLine:
+        continue
 
-print(numberSimilarities)
-print(nameSimilarities)
-
-# # i = int(len(similarities)/2)
-
-# # print(getUser("Brendan Yap"))
-
-# for each in range(int(len(similarities)/2)):
-#     similarPerson = fatData[int(similarities[each])][1]
-#     print(similarPerson)
-#     getUser(0)
-#     # if this plus this divided by index then from same person
-#     # print(f"You and {} both like {}, {}, {}, {}, {}, {}, {}, {}")
-
-#get item num of fatdata sub list that corresponds to value in similarities
-def getItemNum(value):
-    user = 0
-    i = 0
-    x = 0
-    while user == 0:
-        # check for user, get user line num
-        for lines in fatData:
-            x += 1
-            for items in lines:
-                i += 1
-                if value == items:
-                    return i-(x*10)
-    return 0
-
-# for each in range(int(len(numberSimilarities))):
-#     # if each > 0 and each % 2 == 0:
-#     #     eachIndex = each
-#     # elif each > 0 and each % 2 == 0:
-#     #     eachIndex = each-1
-#     # else:
-#     #     eachIndex = 0
-    
-#     similarPerson = fatData[numberSimilarities[each]][getItemNum(nameSimilarities[each])]
-#     similar = 0
-#     for i in range(len(numberSimilarities)):
-#         if nameSimilarities[i] in fatData[userLine]:
-#                 similar += 1
-#     print(f"You have {similar} similarities with {similarPerson}")
-
-# remove similaritie with themselves
-num = len(numberSimilarities)-(len(numberSimilarities)-10)
-for each in range(num):
-    del numberSimilarities[len(numberSimilarities)-1]
-    del nameSimilarities[len(nameSimilarities)-1]
-
-print(numberSimilarities)
-
-numOfSimilarAccumalative = 0
-for each in range(len(numberSimilarities)):
-    #get num of similar
     similar = 0
 
-    # while n
-    #     for items in range(len(numberSimilarities)):
-    #         numOfSimilarAccumalative += numberSimilarities.count(items)
-    
-
-
-    # get 1st count, go the place in list curretnt+1st count, repeat
-
-    for items in numberSimilarities:
-        #count numbers on current index == items
-        if numberSimilarities.count(numberSimilarities[numOfSimilarAccumalative]) == items:
-            numOfSimilarAccumalative += 1
+    for item in range(len(fatData[person])):
+        if fatData[person][item] == fatData[userLine][item]:
             similar += 1
-    
-    # get similar person
-    similarPerson = nameSimilarities[numOfSimilarAccumalative]
 
-    print(f"You have {similar} similarities with {similarPerson}")
+    if similar == 0:
+        continue
+    else:
+        similarPeople.append(fatData[person][1])
+
+    print(f"You have {similar} similarities with {fatData[person][1]}")
+
+#recommended activity part
+"""
+    - get name to be recommendec w/ from list of similar
+"""
+
+# personInput = input("Choose a person for a social interaction reccomendation from the similar people above: ").lower().strip("!?,. ")
+
+# if personInput in 
