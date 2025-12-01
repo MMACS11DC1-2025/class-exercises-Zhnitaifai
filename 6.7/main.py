07ff1dbd1ee975d10ec69dc3a6fb0c0d5b26de81
zhnitaifai <brendanzjy@gmail.com>
2025-12-01 13:35:49 -0800

    Pivoted to April tag detection. Research conducted with partly finished pseudocode.

--- 6.7/main.py
+++ 6.7/main.py
@@ -1,9 +1,48 @@
 '''
-detect extent of cancerous cells (branches), find centriod for use in cancer laser treatment
-steps:
-change image to bipolar
-have list for all cancer cells detected
-for each cancer cells, get see if each of all sides are dark, put in list in list of all cancer cells (crossreference in sublist)
-get average cords within
-round to next cords
+determine id's of 3 different apriltags
+using blob extraction (to account for stray pixels)
+test 1st with 1 tag
+sort by id
+get top right to bottom left dark pixels, seperate tags if gap is more that 5 april tag pictures
 '''
+
+'''
+Connected-component labeling
+when occupioed cell encountered, check neighboring cells to see if scanned.
+
+
+get top left, bottom right, unionizing the blacks
+'''
+
+'''
+give wanted tag
+scan image with multiple tags
+get pattern from list of values of april tags (tag16h5)
+
+area detection (As above)
+make a list (all tags) of lists (per tag) of list (row) of 0 or 1. new line for next row
+get overall size
+divide by expected number of pixels in widh to get pixel width
+match patt
+'''
+from PIL import Image
+# tags r 6x6
+
+referenceTag1 = Image.open("referenceTag1.png").load()
+
+tag16h5 = [
+    [
+     [1, 1, 1, 1, 1, 1],
+     [1, 1, 1, 0, 1, 1],
+     [1, 1, 1, 0, 0, 1],
+     [1, 1, 1, 1, 0, 1],
+     [1, 0, 1, 0, 0, 1],
+     [1, 1, 1, 1, 1, 1]
+    ]
+]
+
+def validation(image, referenceTag):
+    num = 0
+    for y in range(referenceTag.height):
+        for x in range(referenceTag.width):
+        
