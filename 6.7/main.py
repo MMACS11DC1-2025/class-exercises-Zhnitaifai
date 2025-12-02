7c68507e4e02f7c4a2a8a535c787db6929fdd0df
zhnitaifai <brendanzjy@gmail.com>
2025-12-02 13:17:10 -0800

    Full pseudo code written.

--- 6.7/main.py
+++ 6.7/main.py
@@ -15,23 +15,35 @@ get top left, bottom right, unionizing the blacks
 '''
 
 '''
-give wanted tag
-scan image with multiple tags
-get pattern from list of values of april tags (tag16h5)
+2 functions: set april tag, detect april tag
 
-area detection (As above)
-make a list (all tags) of lists (per tag) of list (row) of 0 or 1. new line for next row
-get overall size
-divide by expected number of pixels in widh to get pixel width
-match patt
+1. trim sides until multiple of 6
+2. binarize
+3. breaks down pixels into list for only the current tag
+    break down certain area (width/6) into list
+    for width in area:
+        for height in area:
+            add current pixel to area list
+    after area is scanned, check for avg, add to current tag list
+4. cross reference current tag to tag library
+
+for width:
+    for height:
+        add image[x, y] to 
+
+loop 10 times for each image, input tags to list
+measure time for each loop
+use selection sort for said list
+output report with time in 3 decimal placs (well formatted using string formatting)
 '''
 from PIL import Image
 # tags r 6x6
 
-referenceTag1 = Image.open("referenceTag1.png").load()
+referenceTag1File = Image.open("referenceTag0.png")
+referenceTag1 = referenceTag1File.load()
 
 tag16h5 = [
-    [
+    [ #ID: 0
      [1, 1, 1, 1, 1, 1],
      [1, 1, 1, 0, 1, 1],
      [1, 1, 1, 0, 0, 1],
@@ -42,7 +54,6 @@ tag16h5 = [
 ]
 
 def validation(image, referenceTag):
-    num = 0
     for y in range(referenceTag.height):
         for x in range(referenceTag.width):
-        
+            
